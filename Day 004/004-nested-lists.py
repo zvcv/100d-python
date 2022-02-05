e79d1a767dbdc07007265ae209434de8f58ceb66
Ulinnuha <ulinnuha@dr.com>
2022-02-05 23:43:07 +0700

    added treasure map

---
+++ Day 004/004-nested-lists.py
@@ -0,0 +1,6 @@
+fruits = ["Stoberi", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+veggies = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, veggies]
+
+print (dirty_dozen[0][1])
