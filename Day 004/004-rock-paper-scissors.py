45a9794c9c0a61524cd35f986b994476b10577c7
Ulinnuha <ulinnuha@dr.com>
2022-02-06 00:51:40 +0700

    added 'invalid number' option

--- Day 004/004-rock-paper-scissors.py
+++ Day 004/004-rock-paper-scissors.py
@@ -33,17 +33,21 @@ rps = [rock, paper, scissors]
 
 rps_user = int(input("choose 1 for rock, 2 for paper, or 3 for scissors: "))
 
-print(rps[rps_user - 1])
-
-rpscomp = random.randint(1,3)
-
-print(f"Computer chose: {rpscomp}\n{rps[rpscomp - 1]}")
-
-if (rps_user == 1 and rpscomp == 3) or (rps_user == 2 and rpscomp == 1) or (rps_user == 3 and rpscomp == 2):
-    print("You won.")
-
-elif rps_user == rpscomp:
-    print("Tie game.")
+if (rps_user < 1 or rps_user > 3):
+    print ("whoops, invalid number, you lose!")
 
 else:
-    print("You lose.")
+    print(rps[rps_user - 1])
+
+    rpscomp = random.randint(1,3)
+
+    print(f"Computer chose: {rpscomp}\n{rps[rpscomp - 1]}")
+
+    if (rps_user == 1 and rpscomp == 3) or (rps_user == 2 and rpscomp == 1) or (rps_user == 3 and rpscomp == 2):
+        print("You won.")
+
+    elif rps_user == rpscomp:
+        print("Tie game.")
+
+    else:
+        print("You lose.")
