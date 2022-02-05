f18262e1d0038ee11aac5d01d9c91124b2b2f5c3
Ulinnuha <ulinnuha@dr.com>
2022-02-05 22:53:03 +0700

    code finished and worked

--- Day 003/003-treasure-island.py
+++ Day 003/003-treasure-island.py
@@ -1,3 +1,6 @@
+from turtle import pen
+
+
 print('''
 *******************************************************************************
           |                   |                  |                     |
@@ -23,26 +26,33 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 
-# ask user input of choice left or right
-crossroad = input('You\'re at a crossroad. Where do you want to go? Type "left" or "right"\n')
-
-# if crossroad =
-
-lake = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n')
-
-# if lake =
-
-island = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n")
-
-# if island = 
-
-
-
-
-
-
-
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
 #Write your code below this line 👇
 
+# ask user input of choice left or right
+crossroad = input('You\'re at a crossroad. Where do you want to go? Type "left" or "right"\n')
+
+if crossroad == "left":
+    lake = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n')
+
+    if lake == "wait":
+        island = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n")
+
+        if island == "yellow":
+            print("You found the treasure! You Win!")
+        
+        elif island == "red":
+            print("It's a room full of fire. Game Over.")
+
+        elif island == "blue":
+            print("You enter a room of beasts. Game Over.")
+        
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+    
+    else:
+        print("You get attacked by an angry trout. Game Over.")
+    
+else:
+    print("You fell into a hole. Game Over.")
