c412ed792b86146605c375c894cf6e788171a57f
Ulinnuha <ulinnuha@dr.com>
2022-02-05 22:43:47 +0700

    ask user input about game of choices

--- Day 003/003-treasure-island.py
+++ Day 003/003-treasure-island.py
@@ -21,7 +21,26 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 *******************************************************************************
 ''')
 print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.") 
+print("Your mission is to find the treasure.")
+
+# ask user input of choice left or right
+crossroad = input('You\'re at a crossroad. Where do you want to go? Type "left" or "right"\n')
+
+# if crossroad =
+
+lake = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n')
+
+# if lake =
+
+island = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n")
+
+# if island = 
+
+
+
+
+
+
 
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
